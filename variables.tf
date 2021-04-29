ebdb21406ba16e3bc80d3f2d2ef0a1cc3479e97f
Brad Sickles <brad.sickles@gmail.com>
2021-04-29 10:08:00 -0400

    Initial draft of module

--- variables.tf
+++ variables.tf
@@ -1,3 +1,17 @@
+variable "ssh_public_key" {
+  type        = string
+  description = "The SSH Public Key to authorize for access to the bastion box."
+}
+
+variable "allowed_cidr_blocks" {
+  type        = list(string)
+  default     = []
+  description = <<EOF
+The IP Ranges for users that are allowed to access this bastion from the internet.
+By default, this is empty which allows nobody to access the box.
+EOF
+}
+
 resource "random_string" "resource_suffix" {
   length  = 5
   lower   = true
