1f61cb24169e750662bfcc26b0e57f2a3a563e85
Scott Sickles <scottsickles@gmail.com>
2022-08-22 15:36:06 -0400

    make capabilities.tf the same as aws-fargate-service

--- capabilities.tf
+++ capabilities.tf
@@ -1,10 +1,55 @@
-
-module "caps" {
-  source  = "nullstone-modules/cap-merge/ns"
-  modules = local.modules
-}
-
+// This file is replaced by code-generation using 'capabilities.tf.tmpl'
+// This file helps app module creators define a contract for what types of capability outputs are supported.
 locals {
-  modules       = []
-  capabilities  = module.caps.outputs
+  capabilities = {
+    env = [
+      {
+        name  = ""
+        value = ""
+      }
+    ]
+
+    secrets = [
+      {
+        name  = ""
+        value = ""
+      }
+    ]
+
+    // private_urls follows a wonky syntax so that we can send all capability outputs into the merge module
+    // Terraform requires that all members be of type list(map(any))
+    // They will be flattened into list(string) when we output from this module
+    private_urls = [
+      {
+        url = ""
+      }
+    ]
+
+    // public_urls follows a wonky syntax so that we can send all capability outputs into the merge module
+    // Terraform requires that all members be of type list(map(any))
+    // They will be flattened into list(string) when we output from this module
+    public_urls = [
+      {
+        url = ""
+      }
+    ]
+
+    permissions = [
+      {
+        // required
+        sid_prefix = ""
+        action     = "lambda:InvokeFunction" // lambda:InvokeFunction | lambda:GetFunction
+        principal  = ""
+
+        // optional
+        source_arn             = ""
+        source_account         = ""
+        event_source_token     = ""
+        qualifier              = ""
+        revision_id            = ""
+        principal_org_id       = ""
+        function_url_auth_type = ""
+      }
+    ]
+  }
 }
